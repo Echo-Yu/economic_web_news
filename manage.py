1b1d9eb8fab60acf91ff5131f70ec5b47a4cee4d
Echo Yu <yuhui@163.com>
2018-05-31 12:01:35 +0800

    add mysql and redis

--- manage.py
+++ manage.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from redis import StrictRedis
 
-app = Flask(__name__)
 
 class Config(object):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:mysql@127.0.0.1:3306/economic_news'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
 
+
+app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 
 @app.route('/')
 def index():
