72a6ce2dbf605f79a82a0287fbaaa45587f92f5f
Echo Yu <yuhui@163.com>
2018-05-31 13:48:07 +0800

    add Session

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 from flask_wtf.csrf import CSRFProtect
+from flask_session import Session
 
 
 class Config(object):
@@ -11,6 +12,10 @@ class Config(object):
     REDIS_HOST = '127.0.0.1'
     REDIS_PORT = 6379
     SECRET_KEY = 'ASDFASDFASFDASDF'
+    SESSION_TYPE = 'redis'
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+    SESSION_USE_SIGNER = True
+    PERMANENT_SESSION_LIFETIME = 3600 * 24
 
 
 app = Flask(__name__)
@@ -18,9 +23,12 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 CSRFProtect(app)
+Session(app)
 
 @app.route('/')
 def index():
+    # from flask import session
+    # session['age']='2'
     return 'Hello World!'
 
 if __name__ == '__main__':
