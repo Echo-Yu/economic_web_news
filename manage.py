146671f9f713381dc5085c99bf345959e16d667e
Echo Yu <yuhui@163.com>
2018-05-31 11:30:01 +0800

    add debug

--- manage.py
+++ manage.py
@@ -2,11 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+class Config(object):
+    DEBUG = True
+
+app.config.from_object(Config)
 
 @app.route('/')
 def index():
     return 'Hello World!'
-
 
 if __name__ == '__main__':
     app.run()
