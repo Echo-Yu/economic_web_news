5d718c69202eea7451a186208d75dfa0e1d00d07
Echo Yu <yuhui@163.com>
2018-06-12 23:59:54 +0800

    migrate create table and add data

--- manage.py
+++ manage.py
@@ -2,7 +2,8 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 # from info import app, db
-from info import create_app,db
+# 这里导入models仅仅是为了在迁移时，让manage知道models的存在
+from info import create_app,db,models
 # 创建app
 app = create_app('dev')
 # 创建脚本管理器对象
