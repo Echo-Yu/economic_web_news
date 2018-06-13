905f91b037b449b99dde8fc3498b628584ca842c
Echo Yu <yuhui@163.com>
2018-06-13 09:39:05 +0800

    favicon icon

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,7 +1,12 @@
 from . import index_blue
-from flask import render_template
+from flask import render_template,current_app
 
 @index_blue.route('/')
 def index():
-
+    # 渲染主页
     return render_template('news/index.html')
+
+@index_blue.route('/favicon.ico',methods=['GET'])
+def favicon():
+    # title左侧的图标
+    return current_app.send_static_file('news/favicon.ico')
